93b58e8a32331e5db15bf929165e5c329a4da3b3
Tim Parsons <tim@Tims-MacBook-Pro.local>
2018-10-08 06:37:31 -0700

    tweaked image function

--- src/components/BottleSpinner.js
+++ src/components/BottleSpinner.js
@@ -1,35 +1,44 @@
 import React from 'react';
 import Bottle from '../img/spin_bottle_mockup-4.png';
 
-
-
 /* Create styled div with keyframes and animation function */
+
+const bottleSpinnerSpin = {
+  position: 'relative',
+  top: '-475px',
+  height: '350px'
+}
+
 
 class BottleSpinner extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      animation: false
-    }
-    this.showAlert = this.showAlert.bind(this);
+      animationName: ''
+    };
+    this.rotate = this.rotate.bind(this);
   }
 
-
-  showAlert () {
-    setTimeout(() => {
-      this.setState({
-        animation: !this.state.animation
-      })
-    }, 8000);
+  rotate(){
+    let bottleElem = document.getElementById("bottleElem");
+    let spinTime = (Math.floor(Math.random() * 500) + 1);
+    console.log(spinTime);
+    bottleElem.style.animation = "bottleSpin 10s";
   }
 
+  alert() {
+    alert("Pick a number");    
+  }
 
-  render() { 
+  render() {
     return (
-      <img onClick={this.showAlert} src={Bottle} alt="BottleSpinner"  className={this.state.animation ? "bottleSpinnerSpin" : "bottleSpin"} />
-    )
+      <div>
+       <img style={bottleSpinnerSpin} src={Bottle} onClick={this.rotate} id="bottleElem" alt="spinning-bottle" />
+    </div>
+    )  
   }
 }
+
 
 export default BottleSpinner;
 
